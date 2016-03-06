020cce51b8b070558d7dc84c014b2d78580a6b13
Andrew Brink <abrink27@gmail.com>
2016-03-06 15:09:21 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wmmbpod::Application.config.secret_key_base = '5868eecf4200ac4f00e20794fc1e92495ac9c30a4383d5d4c2390436b8375f90aa01e0f01ac14224bbb3b804d5aa788bf76cf205b3fd85822554ea1d6d039e73'
